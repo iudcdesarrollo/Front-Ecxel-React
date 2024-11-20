caaa1c899545defd3b2d796325923b4a7fdaf80e
Camilocsr <esmunred@gmail.com>
2024-11-20 15:03:41 -0500

    configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        //target: 'http://18.228.211.14:9999',
+        target: import.meta.env.VITE_SERVER_API,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
